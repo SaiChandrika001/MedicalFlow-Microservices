870d40d64c0af5da9b00f6e5ce8d5b0679ef1b36
Sai Chandrika <saichandrika200@gmail.com>
2026-06-02 18:25:33 +0530

    Temporarily disable Kafka integration test

--- appointment-service/src/test/java/com/medicalflow/appointmentservice/service/AppointmentServiceImplTest.java
+++ appointment-service/src/test/java/com/medicalflow/appointmentservice/service/AppointmentServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.medicalflow.appointmentservice.service;
 
+import com.medicalflow.appointmentservice.kafka.AppointmentEventProducer;
 import com.medicalflow.appointmentservice.client.UserServiceClient;
 import com.medicalflow.appointmentservice.dto.AppointmentRequest;
 import com.medicalflow.appointmentservice.dto.AppointmentResponse;
@@ -25,8 +26,8 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -38,6 +39,9 @@ class AppointmentServiceImplTest {
 
     @Mock
     private UserServiceClient userServiceClient;
+
+    @Mock
+    private AppointmentEventProducer appointmentEventProducer;
 
     @InjectMocks
     private AppointmentServiceImpl appointmentService;
